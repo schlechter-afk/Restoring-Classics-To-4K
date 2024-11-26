31ea668513a79b942449121db1a2dee6bfb5d114
Gnana Prakash <gnanaprakashpunnavajhala@gmail.com>
2024-11-26 20:06:29 +0000

    feat: Added SIFT multi-scale features as backbone
    This is similar to the ResNet feature extraction in the sense that we have patch-level SIFT features

--- src/model.py
+++ src/model.py
@@ -16,7 +16,7 @@ from datasets import load_dataset
 from kornia.color import yuv_to_rgb, rgb_to_yuv
 
 from upscaler import Upscaler
-from colorizer import Colorizer
+from alternate_model import Colorizer
 from dataloader import NoisyImageNetDataset
 
 class Restorer(nn.Module):
